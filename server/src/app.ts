0f2befc92e81d39a5239733badc819c3cc13dda8
Izuking <matthiasogbodo@gmail.com>
2019-09-17 19:06:20 +0100

    just finished project setup

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema/schema';
+
+const app = express();
+
+app.disable('x-powered-by');
+app.use(express.json());
+
+// app.use(validateUserSession.bind(this, firebase));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+export default app;
