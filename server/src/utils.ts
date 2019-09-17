d7d76da61021a08bf337cbe7c65fe0ad300e0ce4
Izuking <matthiasogbodo@gmail.com>
2019-09-17 19:25:18 +0100

    adds validator of input

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,7 +1,13 @@
 import joi from '@hapi/joi';
 
-export function doValidation(object: any, schema: joi.SchemaLike) {
-  return joi.validate(object, schema, {
+const validatorSchema = {
+  type: joi.string().required(),
+  margin: joi.number().required(),
+  exchangeRate: joi.number().required(),
+};
+
+export function doValidation(object: any) {
+  return joi.validate(object, validatorSchema, {
     abortEarly: false,
     stripUnknown: true,
   });
