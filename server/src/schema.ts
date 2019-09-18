fc13d54f6e785615026dae7178aed87f65572a97
Izuking <matthiasogbodo@gmail.com>
2019-09-18 08:45:59 +0100

    fixed bugs

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -3,26 +3,70 @@ import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLFloat,
 } from 'graphql';
+import { doValidation } from './utils';
+import fetch from 'node-fetch';
+
+//MAKE CALL TO COIN DESK API END POINT
+async function retrieveBTCPrice() {
+  const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+  try {
+    const response: any = await fetch(url).then(data => data.json());
+
+    const USDPriceObject = response['bpi']['USD'];
+
+    return Number(USDPriceObject['rate_float']);
+  } catch (error) {
+    return error;
+  }
+}
 
 const CalculatePriceObjectType: any = new GraphQLObjectType({
   name: 'CalculatePriceObjectType',
   fields: {
     exchangeType: { type: GraphQLString },
     margin: { type: GraphQLInt },
-    exchangeRate: { type: GraphQLInt },
+    exchangeRate: { type: GraphQLFloat },
+    calculatedPrice: {
+      type: GraphQLString,
+      resolve(parent: any, _args: any) {
+        return parent;
+      },
+    },
   },
 });
-const calculatePrice: any = new GraphQLObjectType({
-  name: 'calculatePrice',
+
+const RootQuery: any = new GraphQLObjectType({
+  name: 'RootQuery',
   fields: {
-    compute: { type: CalculatePriceObjectType },
-    exchangeType: { type: GraphQLString },
-    margin: { type: GraphQLInt },
-    exchangeRate: { type: GraphQLInt },
+    //THIS FIELD DOES THE ACTUAL CALCULATION BASED ON ARGS
+    calculatePrice: {
+      type: CalculatePriceObjectType,
+      args: {
+        exchangeType: { type: GraphQLString },
+        margin: { type: GraphQLFloat },
+        exchangeRate: { type: GraphQLInt },
+      },
+      async resolve(_parent: any, args: any) {
+        const { error, value } = doValidation({ ...args });
+        if (error) {
+          return error;
+        }
+
+        const currentPrice = await retrieveBTCPrice();
+
+        const computedMargin =
+          value.exchangeType === 'BUY'
+            ? currentPrice + value.margin * 100
+            : currentPrice - value.margin * 100;
+
+        return `NGN ${computedMargin * value.exchangeRate}`;
+      },
+    },
   },
 });
 
 export default new GraphQLSchema({
-  query: calculatePrice,
+  query: RootQuery,
 });
