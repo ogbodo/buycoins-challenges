c17c81bc22de6ca8f1a12942c0ce911d62432dc1
Izuking <matthiasogbodo@gmail.com>
2019-09-20 00:47:21 +0100

    minor fix

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,8 @@ import graphqlHTTP from 'express-graphql';
 import schema from './schema';
 
 const app = express();
-
 app.disable('x-powered-by');
 app.use(express.json());
-
 // app.use(validateUserSession.bind(this, firebase));
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
